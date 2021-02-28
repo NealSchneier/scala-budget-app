0c63aa9b57ce884fbb7d86b0b5cd38dc0b85066d
Neal Schneier <nealschneier@ihsmarkit.com>
2021-02-28 13:45:25 -0500

    Income is working as expected

--- src/main/scala/BudgetProgram.scala
+++ src/main/scala/BudgetProgram.scala
@@ -1,16 +1,40 @@
 
 
 object BudgetProgram extends CsvReader {
+  val neal = "Neal-"
+  val lil = "Lil-"
   def main(args: Array[String]): Unit = {
 
     val janItems = parse("transactions-jan-20.csv")
     val income = janItems
-      .filter( x => x.category.toLowerCase.contains("income") || x.category.toLowerCase.contains("Paycheck")) //handle reimbursements
+      .filter( x => x.category.toLowerCase.contains("income")
+        || x.category.toLowerCase.contains("paycheck")
+        || x.category.toLowerCase.contains("reimbursement")) //negate later
+
     val spending = janItems
       .filter( x => !x.category.toLowerCase.contains("income"))
       .filter( x => !x.category.toLowerCase.contains("paycheck"))
       .filter( x => !x.category.toLowerCase.contains("hide from budgets & trends"))//handle reimbursements
+      .filter( x => !x.category.toLowerCase.contains("transfer"))//handle reimbursements
+      .filter( x => !x.category.toLowerCase.contains("reimbursement"))//handle reimbursements
+      .filter( x => !x.category.toLowerCase.contains("investments"))//handle reimbursements
+      .filter( x => !x.category.toLowerCase.contains("Dividend & Cap Gains".toLowerCase())) //Dividend & Cap Gains
 
+    println(s"Total Income ${income.map(x => x.amount).sum}")
+    println(s"Neal Income ${personsItems(income, neal)}")
+    println(s"Lil Income ${personsItems(income, lil)}")
+    println(s"Total Spending ${spending.map(x => x.amount).sum}")
+    println(s"Neal Spending ${personsItems(spending, neal)}")
+    println(s"Lil Spending ${personsItems(spending, lil)}")
+  }
+
+
+  def personsItems(items: List[LineItem], person: String): BigDecimal = {
+    items
+      .filter(x => x.accountName.startsWith(person))
+      .map(x => x.amount)
+      .sum
   }
 }
+
 
--- src/main/scala/CsvReader.scala
+++ src/main/scala/CsvReader.scala
@@ -8,7 +8,9 @@ trait CsvReader {
     for (line <- bufferedSource.getLines()) {
       if (!line.contains("Date")) { // skip the header line
         val cols = line.split("\",\"").map(_.trim.replace("\"", ""))
-        items += LineItem(Utils.convertStringToDate(cols(0)), cols(1), cols(2), BigDecimal(cols(3)), cols(4), cols(5), cols(6), cols(7), cols(8))
+        var lineItem = LineItem(Utils.convertStringToDate(cols(0)), cols(1), cols(2), BigDecimal(cols(3)), cols(4), cols(5), cols(6), cols(7), cols(8))
+        if (lineItem.transactionType.contentEquals("debit")) lineItem = lineItem.copy(amount = -lineItem.amount)
+        items += lineItem
       }
     }
     items.toList
