0511fd79c06a95d2d4ab12c844b92a9681aa8ec0
Neal Schneier <nealschneier@ihsmarkit.com>
2021-04-28 09:32:13 -0400

    Adding adjustments

---
+++ src/main/scala/Adjustments.scala
@@ -0,0 +1,8 @@
+/**
+ * This class is to handle healthcare adjustments.
+ * All adjustments are per month.
+ */
+object Adjustments {
+  val lilAdjustments: Double = 57.86 + 57.86
+  val nealAdjustments: Double = 31 + 270.82 + 128.3 + 14.58 + 3.8
+}
--- src/main/scala/BudgetProgram.scala
+++ src/main/scala/BudgetProgram.scala
@@ -7,12 +7,14 @@ object BudgetProgram extends CsvReader {
 
     val lilPercent = BearsNumbers.percentage("Lil")
     val nealPercent = BearsNumbers.percentage("Neal")
-    val janItems = parse("feb-2021.csv")
+    val janItems = parse("march-2021.csv")
     val income = janItems
       .filter( x => x.category.toLowerCase.contains("income")
         || x.category.toLowerCase.contains("paycheck")
         || x.category.toLowerCase.contains("credit card points")
-        || x.category.toLowerCase.contains("reimbursement")) //negate later
+        || x.category.toLowerCase.contains("reimbursement") //negate later
+        || x.category.toLowerCase.contains("Neal") //Remove Gifts for Neal
+        || x.category.toLowerCase.contains("Lil")) //Remove Gifts for Lil
 
     val spending = janItems
       .filter( x => !x.category.toLowerCase.contains("income"))
@@ -24,10 +26,12 @@ object BudgetProgram extends CsvReader {
       .filter( x => !x.category.toLowerCase.contains("credit card points"))
       .filter( x => !x.category.toLowerCase.contains("Dividend & Cap Gains".toLowerCase())) //Dividend & Cap Gains
       .filter( x => !x.category.toLowerCase.contains("Credit Card Payment".toLowerCase))
+      .filter( x => !x.category.toLowerCase.contains("Neal".toLowerCase))
+      .filter( x => !x.category.toLowerCase.contains("Lil".toLowerCase))
 
-    val nealSpending = personsItems(spending, BearsNumbers.neal)
+    val nealSpending = personsItems(spending, BearsNumbers.neal) + Adjustments.nealAdjustments
     val nealIncome = personsItems(income, BearsNumbers.neal)
-    val lilSpending =personsItems(spending, BearsNumbers.lil)
+    val lilSpending =personsItems(spending, BearsNumbers.lil) + Adjustments.lilAdjustments
     val lilIncome = personsItems(income, BearsNumbers.lil)
     val bearsIncome = personsItems(income, BearsNumbers.bears)
     val totalIncome =income.map(x => x.amount).sum
@@ -42,10 +46,13 @@ object BudgetProgram extends CsvReader {
     println(s"Neal Spending ${nealSpending}")
     println(s"Lil Spending ${lilSpending}")
     println
+    println(s"Neal Adjustments ${Adjustments.nealAdjustments}")
+    println(s"Lil Adjustments ${Adjustments.lilAdjustments}")
+    println
     println(s"Bears Net ${totalIncome + totalSpending}")
     println
-    println(s"Neal Percentage ${nealPercent}")
-    println(s"Lil Percentage ${lilPercent}")
+    println(s"Neal Percentage ${nealPercent.getOrElse(0)}")
+    println(s"Lil Percentage ${lilPercent.getOrElse(0)}")
     println
     println(s"Lil Spending should be ${(totalSpending * lilPercent.getOrElse(0)).setScale(2, BigDecimal.RoundingMode.HALF_UP)}")
     println(s"Neal Spending should be ${(totalSpending * nealPercent.getOrElse(0)).setScale(2, BigDecimal.RoundingMode.HALF_UP)}")
